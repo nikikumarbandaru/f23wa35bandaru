74688d69831320dfe65acdda085b9cb2cf34a3bd
Nikith Kumar Bandaru <nikithkumarbandaru007@gmail.com>
2023-10-25 14:04:26 -0500

    bonus Computation operation

--- routes/bonus.js
+++ routes/bonus.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET Computation operation. */
 router.get('/', function(req, res, next) {
-  var random=Math.floor(Math.random()*100)+1
   var value=parseFloat(req.query.x)
-  var x=!isNaN(value)?value:random
-  var y=Math.cos(x)
-  var output= ('Math.cos()'+' applied to ' +x+' is '+y)
-  res.render('bonus', { response: output });
+  if (value) {
+    var x = parseFloat(value);
+    var y = Math.cos(x); 
+    res.send('Math.cos()'+' applied to ' +x+' is '+y)
+  } else {
+    var x = Math.floor(Math.random() * 100 + 1)
+    var y = Math.cos(x);
+    res.send('Math.cos()'+' applied to ' +x+' is '+y);
+  }
 });
 
 module.exports = router;
+
